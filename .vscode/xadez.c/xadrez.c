d27f2cdf2e1751940ca3d93c58529ab798ff01db
matheusprogramador97 <jonatasmatheus2019@hotmail.com>
2025-06-03 17:38:00 +0000

    tema 3 completo

---
+++ .vscode/xadez.c/xadrez.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void moverTorre(int casa) {
+  if(casa > 0) {
+    printf("DIREITA\n");       //MOVIMENTO DA TORRE (SENTIDO Á DIREITA)
+    moverTorre (casa - 1);
+  }
+}
+ 
+void moverBispo (int casa) {
+  if(casa > 0){
+    printf("CIMA E DIREITA\n");   // MOVIMENTO DO BISPO (SENTIDO CIMA E DIREITA)
+    moverBispo (casa - 1);
+  }
+}
+
+void moverRainha (int casa){
+  if (casa > 0){
+    printf("ESQUERDA\n");       // MOVIMENTO DA RAINHA (SENTIDO Á ESQUERDA)
+    moverRainha( casa - 1);
+  }
+}
+
+int main ()  {
+
+  printf("MOVIMENTO DO CAVALO\n");        //QUANTIDADE DE MOVIMENTO DO CAVALO (3)
+
+  for (int c = 0; c < 1; c++)
+  { 
+    printf("DIREITA\n");                   //MOVIMENTO DO CAVALO (1ª SENTIDO Á DIREITA)
+   {
+   for (int d = 0; d < 2; d++)
+   {
+   printf("CIMA\n");                        // MOVIMENTO DO CAVALO (2ª SENTIDO PARA CIMA )
+       }
+    }
+  }
+  
+  printf("\n");
+
+  printf("MOVIMENTO DA TORRE\n");
+  moverTorre (5);                   //QUANTIDADE DE MOVIMENTO DA TORRE (5)
+  printf("\n");
+  printf("MOVIMENTO DO BISPO\n");
+  moverBispo (8);                  //QUANTIDADE DE MOVIMENTO DO BISPO (8)
+  printf("\n");
+  printf("MOVIMENTO DA RAINHA\n");
+  moverRainha (8);                 //QUNATIDADE DE MOVIMENTO DA RAINHA (8)
+
+  return 0;
+}
